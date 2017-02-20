aa7f3f102e068e0044eda61699cc0f5101007168
Benoit Schillings <benoit.schillings@gmail.com>
2017-02-20 12:56:02 -0800

    still baseline coding

--- skyx.cpp
+++ skyx.cpp
@@ -15,9 +15,16 @@ class Scope {
 public:	
 	float	ra;
 	float	dec;
+	float   alt;
+	float   az;
 
+	void Stop();
+        void Start();
 	void UpdateRaDec();
+	void UpdateAltAz(); 
 	     Scope();
+	
+	void Move(float dx, float dy);		//in arcsec
 };
 
 //---------------------------------------------------
@@ -26,6 +33,78 @@ public:
 {
 	connection.conn((char*)"localhost", 3040);
 }
+
+
+//---------------------------------------------------
+
+
+void Scope::Move(float dx, float dy)
+{
+    char *input = ReadFile("./js/move.txt");
+    connection.send_data(input);
+
+    char *result;
+
+    result = connection.receive(8192);
+
+    printf("%s\n", result);
+    free((char *)input);
+    free((char *)result);
+}
+
+//---------------------------------------------------
+
+
+void Scope::Stop()
+{
+    char *input = ReadFile("./js/stop.txt");
+    connection.send_data(input);
+
+    char *result;
+
+    result = connection.receive(8192);
+       
+    printf("%s\n", result); 
+    free((char *)input);
+    free((char *)result);
+}
+
+//---------------------------------------------------
+
+
+void Scope::Start()
+{
+    char *input = ReadFile("./js/start.txt");
+    connection.send_data(input);
+
+    char *result;
+
+    result = connection.receive(8192);
+       
+    printf("%s\n", result); 
+    free((char *)input);
+    free((char *)result);
+}
+
+//---------------------------------------------------
+
+
+void Scope::UpdateAltAz()
+{
+    char *input = ReadFile("./js/get_alt_az.txt");
+    connection.send_data(input);
+
+    char *result;
+
+    result = connection.receive(8192);
+
+
+    sscanf(result, "%f|%f|", &alt, &az);
+
+    free((char *)input);
+    free((char *)result);
+}
+
 
 //---------------------------------------------------
 
@@ -52,10 +131,19 @@ int main(int argc , char *argv[])
 {
     Scope s;
 
-    s.UpdateRaDec();
+    s.Stop();
+
+    for (int i = 0; i < 200; i++) {
+    	s.UpdateRaDec();
+    	s.UpdateAltAz();
  
-    printf("%f %f\n", s.ra, s.dec); 
-    return 0;
+    	printf("%f %f\n", s.ra, s.dec); 
+    	printf("%f %f\n", s.alt, s.az); 
+    	//if (i % 10 == 0) s.Move(10, 10); 
+     } 
+     s.Stop();
+     
+     return 0;
 }
 
 
