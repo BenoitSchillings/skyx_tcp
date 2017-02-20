ae7ed92ebb28f2fc16cd3098e64479b5d6fa25ec
Benoit Schillings <benoit.schillings@gmail.com>
2017-02-20 11:42:01 -0800

    break down to util.c

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
 all: skyx
 
-sky_ip.o: sky_ip.cpp sky_ip.h 
-	gcc -c sky_ip.cpp -o sky_ip.o
+./obj/sky_ip.o: sky_ip.cpp sky_ip.h 
+	gcc -c sky_ip.cpp -o ./obj/sky_ip.o
 
-skyx: sky_ip.o sky_ip.h skyx.cpp
-	gcc skyx.cpp sky_ip.o -o skyx
+skyx: ./obj/sky_ip.o sky_ip.h skyx.cpp
+	gcc skyx.cpp ./obj/sky_ip.o -o skyx
 
 
--- skyx.cpp
+++ skyx.cpp
@@ -1,6 +1,4 @@
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <netdb.h>
+
 #include <string.h> 
 #include <stdio.h> 
 #include <stdlib.h>
@@ -8,65 +6,55 @@
 
 //---------------------------------------------
 #include "sky_ip.h"
-//---------------------------------------------
+#include "util.c"
 
-char* ReadFile(const char *filename)
+//---------------------------------------------------
+
+class Scope {
+	SkyIP	connection;
+public:	
+	float	ra;
+	float	dec;
+
+	void UpdateRaDec();
+	     Scope();
+};
+
+//---------------------------------------------------
+
+    Scope::Scope()
 {
-    char *buffer = NULL;
-    int string_size, read_size;
-    FILE *handler = fopen(filename, "r");
-
-    if (handler) {
-        fseek(handler, 0, SEEK_END);
-        string_size = ftell(handler);
-        rewind(handler);
-        buffer = (char*) malloc(sizeof(char) * (string_size + 1) );
-        read_size = fread(buffer, sizeof(char), string_size, handler);
-        buffer[string_size] = '\0';
-    
-        if (string_size != read_size) {
-            free(buffer);
-            buffer = NULL;
-        }
-    
-        fclose(handler);
-    }
-
-    return buffer;
+	connection.conn((char*)"localhost", 3040);
 }
 
+//---------------------------------------------------
 
-//---------------------------------------------
+
+void Scope::UpdateRaDec()
+{
+    char *input = ReadFile("./js/get_ra_dec.txt");
+    connection.send_data(input);
+
+    char *result;
+
+    result = connection.receive(8192);
+
+
+    sscanf(result, "%f|%f|", &ra, &dec);
+	
+    free((char *)input);
+    free((char *)result);
+}
+
+//---------------------------------------------------
 
 int main(int argc , char *argv[])
 {
-    char in[512];
-    char *input;
+    Scope s;
 
-
-    SkyIP c;
-
-    c.conn((char*)"localhost" , 3040);
-
-    
-    input = ReadFile("./js/get_ra_dec.txt");
-    c.send_data(input);
-    
-
-    char *result;
-    
-    result = c.receive(8192);
-
-    float ra, dec;
-
-    sscanf(result, "%f|%f|", &ra, &dec);
-
-    printf("%f %f\n",  ra , dec);
-        
-
-    free(result);
-    free(input);
-        
+    s.UpdateRaDec();
+ 
+    printf("%f %f\n", s.ra, s.dec); 
     return 0;
 }
 
